b62066165ea0531325be1c122b1a0b9310992ad3
eleCush <v1nc3ntpull1ng@gmail.com>
2025-06-24 18:18:22 -0400

    unnecessary comment

--- test/vv_otx_service/routes_test.clj
+++ test/vv_otx_service/routes_test.clj
@@ -9,7 +9,6 @@
             [io.pedestal.http :as http]
             [io.pedestal.test :as test]))
 
-;; Test data matching the test-indicators.json structure
 (def test-data
   [{:indicators [{:indicator "85.93.20.243"
                   :description ""
